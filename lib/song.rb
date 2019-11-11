452edfa8984488868ffa8d5c2cf8a8e344f0f926
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-11 16:38:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == nil 
+    if self.find_by_name(name) == nil
       self.create_by_name(name)
     else
       self.find_by_name(name)
